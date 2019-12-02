66c6fc6a8c7a0ec818d2ed2cef1643c21935887a
SAHOWI <wienecke.ch@gmail.com>
2019-12-02 22:15:02 +0100

    fixed minor issues in comments

--- src/tools.cpp
+++ src/tools.cpp
@@ -46,6 +46,8 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
     rmse = rmse.array().sqrt();
     return rmse;
 }
+// END Tools::CalculateRMSE()
+
 
 // M1 = CalculateJacobia(V1)
 MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
@@ -63,21 +65,22 @@ MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
 	double vx = x_state(2);
 	double vy = x_state(3);
 
-	//p re-compute a set of terms to avoid repeated calculation
+	// pre-compute a set of terms to avoid repeated calculation
 	double c1 = px*px + py*py;
 	double c2 = sqrt(c1);
 	double c3 = (c1*c2);
 
-	//check division by zero
+	// check division by zero
 	if(fabs(c1) < 0.0001){
 		cout << "ERROR - CalculateJacobian () - Division by Zero" << endl;
 		return Hj;
 	}
 
-	//compute the Jacobian matrix
+	// finally compute the Jacobian matrix
 	Hj << (px/c2), (py/c2), 0, 0,
 		  -(py/c1), (px/c1), 0, 0,
 		  py*(vx*py - vy*px)/c3, px*(px*vy - py*vx)/c3, px/c2, py/c2;
 
 	return Hj;
 }
+// END Tools::CalculateJcobian()
